72daac18cf34cd916544e3d7e11bfac7d4a25d43
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2019-10-15 17:03:34 +0530

    Create मुख्य.java

---
+++ मुख्य.java
@@ -0,0 +1,20 @@
+public class मुख्य {
+  public static void main(String[] args) {
+    System.out.println("शुरू मुख्य तरीका ");
+    String नाम = "सचिन";
+    दस्तावेज़ दस्तावेज़ = new दस्तावेज़(नाम);
+    System.out.println(दस्तावेज़.प्रतिलिपि());
+  }
+}
+
+class दस्तावेज़ {
+  public String नाम;
+
+  public दस्तावेज़(String नाम) {
+    this.नाम = नाम;
+  }
+
+  public String प्रतिलिपि() {
+    return "दस्तावेज़ [नाम=" + नाम + "]";
+  }
+}
